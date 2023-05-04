7daedd264bbbc876cf4b652d343eb7862a252d6e
Angel <abstract.data100@gmail.com>
2023-05-04 00:15:30 -0600

    layouts

---
+++ LayoutsJava2208/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //Ventana ventana = new Ventana("Layouts");
+        //VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layout cuadricula");
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Farenheid");
+
+    }
+}
